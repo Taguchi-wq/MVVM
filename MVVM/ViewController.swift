3b7d5175adfc5aca1107e4d54bbfc9bd7038415d
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-18 14:54:42 +0900

    tableViewを実装

--- MVVM/ViewController.swift
+++ MVVM/ViewController.swift
@@ -11,11 +11,29 @@ class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView(tableView)
     }
 
+    private func setupTableView(_ tableView: UITableView) {
+        tableView.dataSource = self
+    }
 
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Anyone"
+        return cell
+    }
+    
 }
 
